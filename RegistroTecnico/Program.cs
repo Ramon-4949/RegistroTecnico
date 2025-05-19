75ba809793952a429a90581fd41eb16426dc9d97
Ramon Gonzalez <ramon_gonzalez2@ucne.edu.do>
2025-05-19 00:21:22 -0400

    Modificando Componentes necesarios

--- RegistroTecnico/Program.cs
+++ RegistroTecnico/Program.cs
@@ -1,24 +1,34 @@
+using Microsoft.EntityFrameworkCore;
 using RegistroTecnico.Components;
+using RegistroTecnico.DAL;
+using RegistroTecnico.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Obtener la cadena de conexión desde appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("SqlConStr");
+
+// Registrar el DbContext principal
+builder.Services.AddDbContext<ContextoPrueba>(options =>
+    options.UseSqlServer(connectionString));
+
+// Registrar servicios
+builder.Services.AddScoped<TecnicosService>();
+
+// Configuración de Razor Components (Blazor Server)
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Middleware de errores y seguridad
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
