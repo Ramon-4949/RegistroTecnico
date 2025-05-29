3912ccd8989a90a3a57da16f263b12f1ff2fd710
Ramon Gonzalez <ramon_gonzalez2@ucne.edu.do>
2025-05-29 10:33:40 -0400

    Crear Modelo de Tickets

---
+++ RegistroTecnico/Models/Tickets.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnico.Models
+{
+    public class Tickets
+    {
+        [Key]
+        public int TicketId { get; set; }
+        public DateOnly Fecha { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        [Required(ErrorMessage ="Este campo es requerido")]
+        public string Prioridad { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public int ClienteId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Asunto { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public double TiempoInvertido { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public int TecnicoId { get; set; }
+
+        public Clientes Cliente { get; set; }
+        public Tecnicos Tecnico { get; set; }
+
+    }
+}
